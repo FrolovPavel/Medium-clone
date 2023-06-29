8775039680359d1023d48723d478898bbc3697ef
Pavel Frolov <Frolov-Works@yandex.ru>
2023-06-30 02:01:18 +0300

    feat: add register page

---
+++ src/components/AppInput.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "AppInput"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/Register.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="signup">
+    <div class="container">
+      <h1 class="signup__title">Sign in</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppRegister'
+}
+</script>
+
+<style scoped lang="scss">
+@import '../assets/scss/vars';
+
+.signup {
+
+  &__title {
+    color: $dark;
+    font-weight: 400;
+    font-size: 2.5rem;
+    text-align: center;
+  }
+}
+</style>
